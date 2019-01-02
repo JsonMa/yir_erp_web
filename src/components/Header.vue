1a526072dc968de87206a8bd9a2b78011d4bea09
JsonMa <mahao@iot.chinamobile.com>
2019-01-02 17:41:13 +0800

    fix: 修改基础样式

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,25 @@
 <template>
   <header class="header">
-   这是头部
+    <el-row>
+      <el-col :span="4">
+        <router-link :to="{ name: 'cooperator', params: { userId: 123 }}">供应商管理</router-link>
+      </el-col>
+      <el-col :span="4">
+        <router-link :to="{ name: 'material', params: { userId: 123 }}">原材料管理</router-link>
+      </el-col>
+      <el-col :span="4">
+        <router-link :to="{ name: 'product', params: { userId: 123 }}">成品管理</router-link>
+      </el-col>
+      <el-col :span="4">
+        <router-link :to="{ name: 'wasted', params: { userId: 123 }}">废料管理</router-link>
+      </el-col>
+      <el-col :span="4">
+        <router-link :to="{ name: 'order', params: { userId: 123 }}">订单管理</router-link>
+      </el-col>
+      <el-col :span="4">
+        <router-link :to="{ name: 'user', params: { userId: 123 }}">人员管理</router-link>
+      </el-col>
+    </el-row>
   </header>
 </template>
 
@@ -14,18 +33,11 @@ export default {
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.header {
+  height: 80px;
+  line-height: 80px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 </style>
