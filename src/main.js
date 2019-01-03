359c739b3b2a784bc447cebf20603fdb68d5b7a6
JsonMa <mahao@iot.chinamobile.com>
2019-01-03 17:36:57 +0800

    fix: 修改原材料管理页面

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './element-variables.scss'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios.create({
+  baseURL: 'http://127.0.0.1:7001/api',
+  timeout: 1000
+})
 
 new Vue({
   router,
--- src/pages/material/Material.vue
+++ src/pages/material/Material.vue
@@ -3,12 +3,25 @@
     <div class="searching">
       <el-form :inline="true" :model="searchingForm" class="searching-form">
         <el-form-item>
+          <el-select
+            v-model="searchingForm.status"
+            placeholder="入库单状态"
+            class="materil-entry-status"
+          >
+            <el-option label="所有" value></el-option>
+            <el-option label="未审核" value="UNREVIEW"></el-option>
+            <el-option label="通过" value="PASSED"></el-option>
+            <el-option label="未通过" value="REJECTED"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
           <el-date-picker
             v-model="searchingForm.dateTime"
             type="datetimerange"
             range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
+            :picker-options="dateTimeOptions"
           ></el-date-picker>
         </el-form-item>
         <el-form-item>
@@ -24,57 +37,38 @@
       <el-table :data="materialEntries" style="width: 100%">
         <el-table-column type="expand">
           <template slot-scope="props">
-            <el-form
-              label-position="left"
-              inline
-              class="demo-table-expand"
-              v-loading="props.row.showLoading"
-            >
-              <el-form-item label="序号">
-                <span>{{ props.row.index }}</span>
+            <el-form label-position="left" inline class="table-expand">
+              <el-form-item label="厂家：">
+                <span>{{ props.row.material.supplier }}</span>
               </el-form-item>
-              <el-form-item label="入库单编号">
-                <span>{{ props.row.shop }}</span>
+              <el-form-item label="材料编码：">
+                <span>{{ props.row.material.no }}</span>
               </el-form-item>
-              <el-form-item label="材料编号">
-                <span>{{ props.row.id }}</span>
+              <el-form-item label="采购员：">
+                <span>{{ props.row.buyer.name }}</span>
               </el-form-item>
-              <el-form-item label="材料名称">
-                <span>{{ props.row.shopId }}</span>
+              <el-form-item label="质检员：">
+                <span>{{ props.row.inspector.name }}</span>
               </el-form-item>
-              <el-form-item label="型号">
-                <span>{{ props.row.category }}</span>
+              <el-form-item label="质检结果：">
+                <span>{{ props.row.quality_result ? '通过' : '不通过' }}</span>
               </el-form-item>
-              <el-form-item label="厂家">
-                <span>{{ props.row.address }}</span>
-              </el-form-item>
-              <el-form-item label="单位">
-                <span>{{ props.row.desc }}</span>
-              </el-form-item>
-              <el-form-item label="入库数量">
-                <span>{{ props.row.desc }}</span>
-              </el-form-item>
-              <el-form-item label="入库金额">
-                <span>{{ props.row.desc }}</span>
-              </el-form-item>
-              <el-form-item label="入库日期">
-                <span>{{ props.row.desc }}</span>
-              </el-form-item>
-              <el-form-item label="制单人">
-                <span>{{ props.row.desc }}</span>
+              <el-form-item label="付款方式：">
+                <span>{{ props.row.purchase_method }}</span>
               </el-form-item>
             </el-form>
           </template>
         </el-table-column>
         <el-table-column label="序号" prop="index" width="60px"></el-table-column>
         <el-table-column label="入库单编号" prop="no"></el-table-column>
-        <el-table-column label="材料编号" prop="material.no"></el-table-column>
         <el-table-column label="材料名称" prop="material.name"></el-table-column>
         <el-table-column label="型号" prop="material.model"></el-table-column>
         <el-table-column label="入库数量" prop="real_count"></el-table-column>
+        <el-table-column label="单位" prop="unit"></el-table-column>
         <el-table-column label="入库金额" prop="total_price"></el-table-column>
         <el-table-column label="入库日期" prop="created_at"></el-table-column>
         <el-table-column label="制单人" prop="maker.name" width="80px"></el-table-column>
+        <el-table-column label="审核状态" prop="status" width="80px"></el-table-column>
         <el-table-column label="操作" width="200px">
           <template slot-scope="scope">
             <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">修改</el-button>
@@ -84,16 +78,16 @@
       </el-table>
     </div>
 
-    <div class="paging">
+    <div class="paging" v-if="materialEntries.length">
       <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page.sync="currentPage3"
-        :page-size="1"
+        :current-page.sync="page.currentPage"
+        :page-size="page.pageSize"
         background
         big
-        layout="prev, pager, next, jumper"
-        :total="100"
+        layout="total, prev, pager, next, jumper"
+        :total="page.total"
         class="paging-item"
       ></el-pagination>
     </div>
@@ -101,55 +95,105 @@
 </template>
 
 <script>
+const moment = require('moment-timezone')
 export default {
   name: 'Material',
   data () {
     return {
       searchingForm: {
         dateTime: '',
-        keyword: ''
+        keyword: '',
+        status: ''
       },
-      materialEntries: [
-        {
-          index: 10000,
-          no: '入库单序号',
-          material: {
-            no: '材料序号',
-            name: '材料名称',
-            model: '材料型号'
+      materialEntries: [],
+      page: {
+        currentPage: 1,
+        pageSize: 1,
+        total: 0
+      },
+      dateTimeOptions: {
+        shortcuts: [
+          {
+            text: '今天',
+            onClick (picker) {
+              const end = new Date()
+              const start = new Date(moment().format('YYYY-MM-DD'))
+              start.setTime(start.getTime() - 3600 * 1000 * 8)
+              picker.$emit('pick', [start, end])
+            }
           },
-          unit: '件',
-          real_count: 100,
-          total_price: 20000,
-          created_at: '2018-12-29T09:25:56.916Z',
-          maker: {
-            name: '制作者'
-          }
-        },
-        {
-          index: 10001,
-          no: '入库单序号',
-          material: {
-            no: '材料序号',
-            name: '材料名称',
-            model: '材料型号'
+          {
+            text: '最近一周',
+            onClick (picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+              picker.$emit('pick', [start, end])
+            }
           },
-          unit: '件',
-          real_count: 100,
-          total_price: 20000,
-          created_at: '2018-12-29T09:25:56.916Z',
-          maker: {
-            name: '制作者'
+          {
+            text: '最近一个月',
+            onClick (picker) {
+              const end = new Date()
+              const start = new Date()
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+              picker.$emit('pick', [start, end])
+            }
           }
-        }
-      ],
-      expandRecord: {} // 记录展开状态
+        ]
+      }
     }
   },
+
   methods: {
-    searching () {
-      alert('111')
+    searching (isNewSearch = true) {
+      const { keyword, dateTime, status } = this.searchingForm
+      if (isNewSearch) this.page.currentPage = 1
+      this.getMaterialEntries({
+        status,
+        keyword,
+        start_time: dateTime[0],
+        end_time: dateTime[1]
+      })
+    },
+
+    handleSizeChange () {},
+
+    handleCurrentChange (currentPage) {
+      console.log(currentPage)
+      this.page.currentPage = currentPage
+      this.searching(false)
+    },
+
+    // 数据统一处理函数
+    getMaterialEntries (params) {
+      const { pageSize, currentPage } = this.page
+      const defaultParams = {
+        limit: pageSize,
+        offset: pageSize * (currentPage - 1)
+      }
+      this.$axios
+        .get('/material_entries', {
+          params: Object.assign(defaultParams, params)
+        })
+        .then(res => {
+          const { data, meta } = res.data
+          const { currentPage, pageSize } = this.page
+          this.materialEntries = data.map((item, index) => {
+            item.index = (currentPage - 1) * pageSize + index + 1
+            item.no = item.no.split('-')[0]
+            item.created_at = moment
+              .tz(item.created_at, 'Asia/Shanghai')
+              .format('YYYY-MM-DD HH:mm:ss')
+            return item
+          })
+          this.page.total = meta.count
+        })
     }
+  },
+
+  created () {
+    this.getMaterialEntries()
   }
 }
 </script>
@@ -172,7 +216,14 @@ export default {
     padding-top: 20px;
     .keyword {
       .el-input__inner {
-        width: 681px;
+        width: 454px;
+      }
+    }
+    .materil-entry-status {
+      .el-input--suffix {
+        .el-input__inner {
+          width: 150px;
+        }
       }
     }
   }
